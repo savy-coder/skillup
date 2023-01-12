e49295b4b6b655b74d158ff689ac88dfe7924dac
savy-coder <81430219+savy-coder@users.noreply.github.com>
2023-01-12 23:19:50 +0500

    Navbar and toggle menu

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,17 +1,50 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './navbar.css';
-// import { RiMenu3Line, RiCloseLin } from 'react-icons/ri';
-import logo from '../../assets/logo.svg';
+import { RiMenu3Line, RiCloseLine } from 'react-icons/ri';
+import logo from '../../assets/logo.png';
 
 // BEM -> Block Element Modifier
 
 const Navbar = () => {
+ const Menu = () => (
+  <>
+  <p><a href="#home">Home</a></p>
+  <p><a href="#wskillup">What is Skillup?</a></p>
+  <p><a href="#possibility">Open AI</a></p>
+  <p><a href="#features">Case Studies</a></p>
+  <p><a href="#blog">Library</a></p>
+  </>
+ )
+ const [toggleMenu, setToggleMenu] = useState(false);
   return (
     <div className="skillup__navbar">
       <div className="skillup__navbarlinks">
-        <div className="skillup__navbar-links_logo">
+        <div className="skillup__navbarlinks_logo">
           <img src={logo} alt="logo" />
         </div>
+        <div className="skillup__navbarlinks_container">
+          <Menu />
+        </div>
+      </div>
+      <div className="skillup__navbar-sign">
+        <p>Sign In</p>
+        <button type="button">Sign Up</button>
+      </div>
+      <div className="skillup__navbar-menu">
+      {toggleMenu
+          ? <RiCloseLine color="#fff" size={27} onClick={() => setToggleMenu(false)} />
+          : <RiMenu3Line color="#fff" size={27} onClick={() => setToggleMenu(true)} />}
+      {toggleMenu && (
+        <div className="skillup__navbar-menu_container scale-up-center">
+          <div className="skillup_navbar-menu_container-links">
+            <Menu />
+            <div className="skillup__navbar-menu_container-links-sign">
+              <p>Sign In</p>
+              <button type="button">Sign Up</button>
+            </div>
+          </div>
+        </div>
+      )}
       </div>
     </div>
   )
