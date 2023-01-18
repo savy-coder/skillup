7d058d37f6ac40d1a7ca280648a91fb957186b5e
savy-coder <81430219+savy-coder@users.noreply.github.com>
2023-01-18 08:59:07 +0500

    Skillup header initial customizations

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -1,10 +1,29 @@
 import React from 'react';
 import './header.css';
+import people from '../../assets/people.png';
+import ai from '../../assets/ai.png';
 
 const Header = () => {
   return (
-    <div>
-      Header
+    <div className="skillup__header section__padding" id="home">
+      <div className="skillup__header-content">
+        <h1 className="gradient__text">Let's Build Something Amazing with React JS</h1>
+        <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English.</p>
+
+        <div className="skillup__header-content__input">
+          <input type="email" placeholder="Your Email Address" />
+          <button type="button">Get Started</button>
+        </div>
+
+        <div className="skillup__header-content__people">
+          <img src={people} alt="people" />
+          <p>1,600 people requested access a visit in last 24 hours</p>
+        </div>
+
+        <div className="skillup__header-image">
+          <img src={ai} alt="ai" />
+        </div>
+      </div>
     </div>
   )
 }
