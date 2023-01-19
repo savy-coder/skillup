61319d8ffe561773d3603278de982cdd7571df06
savy-coder <81430219+savy-coder@users.noreply.github.com>
2023-01-19 12:44:10 +0000

    Header, Brand & Features Integrated

--- src/components/feature/Feature.jsx
+++ src/components/feature/Feature.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import './feature.css';
 
-const Feature = () => {
+const Feature = ( {title, text} ) => {
   return (
-    <div>
-      Feature
+    <div className="skillup-features-container__feature">
+      <div className="skillup__features-container-title">
+        <div />
+        <h1>{title}</h1>
+      </div>
+      <div className="skillup__features-container_feature-text">
+        <p>{text}</p>
+      </div>
     </div>
   )
 }
--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -7,7 +7,7 @@ const Header = () => {
   return (
     <div className="skillup__header section__padding" id="home">
       <div className="skillup__header-content">
-        <h1 className="gradient__text">Let's Build Something Amazing with React JS</h1>
+        <h1 className="gradient__text">Let's Build Something Amazing with React JS OpenAI</h1>
         <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English.</p>
 
         <div className="skillup__header-content__input">
@@ -19,10 +19,9 @@ const Header = () => {
           <img src={people} alt="people" />
           <p>1,600 people requested access a visit in last 24 hours</p>
         </div>
-
-        <div className="skillup__header-image">
+      </div>
+      <div className="skillup__header-image">
           <img src={ai} alt="ai" />
-        </div>
       </div>
     </div>
   )
